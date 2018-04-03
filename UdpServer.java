0d4fea3d34429c74f4cc37e5b828f71338fc32ef
geet <geeethu94@gmail.com>
2018-04-03 14:44:03 +0530

    third commit

--- UdpServer.java
+++ UdpServer.java
@@ -4,3 +4,6 @@ class UDPServer
 {
    public static void main(String args[]) throws Exception
       {
+		DatagramSocket serverSocket = new DatagramSocket(9876);
+            byte[] receiveData = new byte[1024];
+            byte[] sendData = new byte[1024];
